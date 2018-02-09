33deab53885bec68f29e701f532805b306c57791
AmitaRashyani <36245415+AmitaRashyani@users.noreply.github.com>
2018-02-09 00:05:15 +0000

    Add files via upload

---
+++ SearchNUnit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace FirstTestCase
+{
+    class SearchNUnit
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+
+        }
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://www.sword-activerisk.com";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+
+        }
+
+    }
+}
+
